57670e5dbe4804bfb00c0bd6e0f453473c69bb4c
Stanley Chin <schin42@uwo.ca>
2015-02-26 11:53:51 -0500

    tried to add refresh button

--- WeatherProject/src/WeatherFrame.java
+++ WeatherProject/src/WeatherFrame.java
@@ -1,11 +1,23 @@
 import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
-public class WeatherFrame extends JFrame {
+
+public class WeatherFrame extends JFrame implements ActionListener {
+	 
 	public WeatherFrame()
 	{
 		setTitle("Team 20 Weather App");
 		setSize(500, 400); 
 		setLocation(100, 200);
+		
+		JButton refreshButton = new JButton("Refresh");
+		refreshButton.setBounds(40, 50, 40, 50);
+		add(refreshButton);
+		refreshButton.addActionListener(this);
 	}
 	
 	@SuppressWarnings("deprecation")
@@ -13,5 +25,10 @@ public class WeatherFrame extends JFrame {
 	{
 		JFrame f = new WeatherFrame();
 		f.show();
+	}
+	
+	public void actionPerformed(ActionEvent e)
+	{
+		
 	}
 }
