c9c1d053b295b1a6a17ff24fe660a66f3a3d39b8
Stanley Chin <schin42@uwo.ca>
2015-02-28 16:17:40 -0500

    added unit class for ease of conversion

--- WeatherProject/src/Unit.java
+++ WeatherProject/src/Unit.java
@@ -1,8 +1,114 @@
 
 public class Unit {
+	private static final String KELVIN_UNIT = "kelvin";
+	private static final String CELSIUS_UNIT = "celsius";
+	private static final String FAHRENHEIT_UNIT = "fahrenheit";
+	private static final double KELVIN_CONSTANT = 273.15;
+	private String from_Unit;
+	private String to_Unit;
+	String current_unit;
+	double temperature;
 	
-	public Unit()
+	public Unit(double temp, String unit)
 	{
-		
+		this.temperature = temp;
+		this.current_unit = unit; 
 	}
+
+	public void changeUnits(String unit_to_change_to)
+	{
+		this.from_Unit = current_unit;
+		this.to_Unit = unit_to_change_to;
+		convertTemperature();
+	}
+	/**
+	 * convertTemperature converts the temperature from Celsius, Kelvin or Fahrenheit to its corresponding temperature
+	 * @param from_Unit,to_Unit,temp 
+	 * @return temp the temperature that has been converted
+	 */
+	private void convertTemperature()
+	{
+		if(kelvinToCelsius())
+			temperature=K_to_C();
+		else if(celsiusToKelvin()) 
+			temperature=C_to_K();
+		else if(kelvinToFahrenheit()) 
+		{
+			temperature=K_to_C();
+			temperature=C_to_F();
+		}
+		else if(fahrenheitToKelvin())
+		{
+			temperature=F_to_C();
+			temperature=C_to_K();
+		}
+		else if(fahrenheitToCelsius())
+			temperature=F_to_C();
+		else if(celsiusToFahrenheit()) //celsiusToFahrenheit 
+			temperature=C_to_F();	
+	}
+	
+	private boolean kelvinToCelsius()
+	{
+		if(from_Unit.equals(KELVIN_UNIT) && to_Unit.equals(CELSIUS_UNIT))
+			return true;
+		else
+			return false;
+	}
+	private double K_to_C()
+	{
+		temperature = temperature - KELVIN_CONSTANT;
+		return temperature;
+	}
+	private boolean celsiusToKelvin()
+	{
+		if(from_Unit.equals(CELSIUS_UNIT) && to_Unit.equals(KELVIN_UNIT)) 
+			return true;
+		else
+			return false;
+	}
+	private double C_to_K()
+	{
+		temperature = temperature + KELVIN_CONSTANT;
+		return temperature;
+	}
+	private boolean kelvinToFahrenheit()
+	{
+		if(from_Unit.equals(KELVIN_UNIT) && to_Unit.equals(FAHRENHEIT_UNIT)) 
+			return true;
+		else
+			return false;
+	}
+	private boolean fahrenheitToKelvin()
+	{
+		if(from_Unit.equals(FAHRENHEIT_UNIT) && to_Unit.equals(KELVIN_UNIT))
+			return true;
+		else
+			return false;
+	}
+	private boolean fahrenheitToCelsius()
+	{
+		if(from_Unit.equals(FAHRENHEIT_UNIT) && to_Unit.equals(CELSIUS_UNIT))
+			return true;
+		else
+			return false;
+	}
+	private double F_to_C()
+	{
+		temperature = (temperature - 32.0)/(9.0/5.0);
+		return temperature;
+	}
+	private boolean celsiusToFahrenheit()
+	{
+		if(from_Unit.equals(CELSIUS_UNIT) && to_Unit.equals(FAHRENHEIT_UNIT))
+			return true;
+		else
+			return false;
+	}
+	private double C_to_F()
+	{
+		temperature = temperature * (9.0/5.0) + 32.0;
+		return temperature;
+	}
+	
 }
--- WeatherProject/src/WeatherData.java
+++ WeatherProject/src/WeatherData.java
@@ -3,25 +3,34 @@
  * @author Team 20
  */
 
-public class WeatherData{
+public class WeatherData extends Unit{
 
 	/* Instance Variables */
-	private double temperature;
+	//private double temperature;
 	private double windSpeed;
 	private double windDirection;
 	private double airPressure;
 	private double humidity;
 	private int skyConditionID;
-	private double minTemp;
-	private double maxTemp;
+	//private double minTemp;
+	//private double maxTemp;
 	private double sunRise;
 	private double sunSet;
-	private static final String KELVIN_UNIT = "kelvin";
-	private static final String CELSIUS_UNIT = "celsius";
-	private static final String FAHRENHEIT_UNIT = "fahrenheit";
-	private static final double KELVIN_CONSTANT = 273.15;
-
-	/* Constructor */
+	private	double temperature;
+	private double minTemp;
+	private double maxTemp;
+	
+	/*
+	 * Constructor for WeatherData class.
+	 * Initializes the instance variables with the first fetch-data from the source 
+	 
+	public WeatherData(){
+		
+	}*/
+	
+	/*
+	 * this class returns a list of WeatherData elements that updates the data by fetching it from the source 
+	 */
 	public WeatherData update()
 	{
 		return this;
@@ -31,13 +40,19 @@ public class WeatherData{
 
 	/**
 	 * changeTemperature method changes the temperature
-	 * @param fromUnit is the first unit, toUnit is the second unit
+	 * @param fromUnit is the unit to be changed from, toUnit is the unit to change into 
 	 */
-	public void changeTemperature(String fromUnit, String toUnit)
+	public void changeTemperatureUnit(String currentUnit, String newUnit)
 	{
-		temperature = convertTemperature(fromUnit,toUnit,temperature);
-		minTemp = convertTemperature(fromUnit,toUnit,minTemp);
-		maxTemp = convertTemperature(fromUnit,toUnit,maxTemp);
+		Unit temp_1 = new Unit(temperature, currentUnit);
+		Unit unit_Min = new Unit(minTemp, currentUnit);
+		Unit unit_Max = new Unit(maxTemp, currentUnit);
+		temp_1.changeUnits(newUnit);
+		unit_Min.changeUnits(newUnit);
+		unit_Max.changeUnits(newUnit);
+		temperature = temp_1.temperature;
+		minTemp = unit_Min.temperature;
+		maxTemp = unit_Max.temperature;
 	}
 	
 	/*================================================================================================================
@@ -69,95 +84,6 @@ public class WeatherData{
 	================================================================================================================*/
 	
 	
-	/**
-	 * convertTemperature converts the temperature from celsius, kelvin or fahrenheit to its corresponding temperature
-	 * @param fromUnit,toUnit,temp 
-	 * @return temp the temperature that has been converted
-	 */
-	private static double convertTemperature(String fromUnit, String toUnit, double temp)
-	{
-		if(kelvinToCelsius(fromUnit, toUnit))
-			temp=K_to_C(temp);
-		else if(celsiusToKelvin(fromUnit, toUnit)) 
-			temp=C_to_K(temp);
-		else if(kelvinToFahrenheit(fromUnit, toUnit)) 
-		{
-			temp=K_to_C(temp);
-			temp=C_to_F(temp);}
-		else if(fahrenheitToKelvin(fromUnit, toUnit))
-		{
-			temp=F_to_C(temp);
-			temp=C_to_K(temp);
-		}
-		else if(fahrenheitToCelsius(fromUnit, toUnit))
-			temp=F_to_C(temp);
-		else if(celsiusToFahrenheit(fromUnit, toUnit)) //celsiusToFahrenheit 
-			temp=C_to_F(temp);
-		
-		return temp;
-	}
 	
-	private static boolean kelvinToCelsius(String fromUnit, String toUnit)
-	{
-		if(fromUnit.equals(KELVIN_UNIT) && toUnit.equals(CELSIUS_UNIT))
-			return true;
-		else
-			return false;
-	}
-	private static double K_to_C(double temp)
-	{
-		temp = temp - KELVIN_CONSTANT;
-		return temp;
-	}
-	private static boolean celsiusToKelvin(String fromUnit, String toUnit)
-	{
-		if(fromUnit.equals(CELSIUS_UNIT) && toUnit.equals(KELVIN_UNIT)) 
-			return true;
-		else
-			return false;
-	}
-	private static double C_to_K(double temp)
-	{
-		temp = temp + KELVIN_CONSTANT;
-		return temp;
-	}
-	private static boolean kelvinToFahrenheit(String fromUnit, String toUnit)
-	{
-		if(fromUnit.equals(KELVIN_UNIT) && toUnit.equals(FAHRENHEIT_UNIT)) 
-			return true;
-		else
-			return false;
-	}
-	private static boolean fahrenheitToKelvin(String fromUnit, String toUnit)
-	{
-		if(fromUnit.equals(FAHRENHEIT_UNIT) && toUnit.equals(KELVIN_UNIT))
-			return true;
-		else
-			return false;
-	}
-	private static boolean fahrenheitToCelsius(String fromUnit, String toUnit)
-	{
-		if(fromUnit.equals(FAHRENHEIT_UNIT) && toUnit.equals(CELSIUS_UNIT))
-			return true;
-		else
-			return false;
-	}
-	private static double F_to_C(double temp)
-	{
-		temp = (temp - 32.0)/(9.0/5.0);
-		return temp;
-	}
-	private static boolean celsiusToFahrenheit(String fromUnit, String toUnit)
-	{
-		if(fromUnit.equals(CELSIUS_UNIT) && toUnit.equals(FAHRENHEIT_UNIT))
-			return true;
-		else
-			return false;
-	}
-	private static double C_to_F(double temp)
-	{
-		temp = temp * (9.0/5.0) + 32.0;
-		return temp;
-	}
 }
 
