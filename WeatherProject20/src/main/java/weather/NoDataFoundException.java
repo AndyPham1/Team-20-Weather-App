11f6887fab00f3fe10b02b19980d0ef56782ef57
Stanley Chin <schin42@uwo.ca>
2015-03-11 11:22:15 -0400

    removed old WeatherProject, added new one

---
+++ WeatherProject20/src/main/java/weather/NoDataFoundException.java
@@ -0,0 +1,14 @@
+package weather;
+
+/**
+ * NoDataFoundException is an exception class
+ * @author Team 20
+ */
+
+public class NoDataFoundException extends Exception {
+	
+	   public NoDataFoundException(String message)
+	   {
+	      super ("No Data Found " + message);
+	   }
+}
